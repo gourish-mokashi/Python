9494ca1ea079f756c6efb5f367b0cff0c520b494
Gourish <gourishvinayakmokashi@gmail.com>
2022-06-18 11:37:22 +0530

    my python projects

---
+++ Dictionary exercise.py
@@ -0,0 +1,11 @@
+# Author Gourish Mokashi
+# 18/06/2022
+
+# Creating a Dictionary which will be taking input from user and provide the definition
+
+dict1 = {"Apple": "A Fruit", "Mobile": "A device which makes our work easy", "School": "A place where students learn, "
+                                                                                       "write and study",
+         "Append": "At the end"}
+print(" Enter the word")
+dict2 = input()
+print(dict1[dict2])
