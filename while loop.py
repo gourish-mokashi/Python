b54ac50fc16df1b396dc070a2e8ee6a388fd9389
Gourish <gourishvinayakmokashi@gmail.com>
2022-06-25 22:41:05 +0530

    my python projects

---
+++ while loop.py
@@ -0,0 +1,12 @@
+# Author Gourish Mokashi
+# 25/06/2006
+
+# About while loop
+
+i = 1
+
+while i <= 40:
+    print(i)
+    # while loop is same as for loop or if loop
+    i = i + 1
+# here by just implementing this we can set limit
