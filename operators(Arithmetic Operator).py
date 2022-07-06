3f831e0a772f0778074ddafb3c23162f9d03d6b3
Gourish <gourishvinayakmokashi@gmail.com>
2022-07-06 22:41:52 +0530

    my python projects

---
+++ operators(Arithmetic Operator).py
@@ -0,0 +1,37 @@
+# Author Gourish mokashi
+# 03/07/2022
+
+# All about operators
+"""Types of operators
+Arithmetic Operator
+Assignment Operators
+Comparison Operators
+Logical Operators
+Identity Operators
+Membership Operators
+Bitwise Operators
+"""
+
+# Arithmetic Operator
+
+print("5 + 6 is ", 5+6)
+# + operator is used to add 2
+
+print("5 - 6 is ", 5-6)
+# - operator is used to subtract 2 integers
+
+print("5 * 6 is ", 5*6)
+# * operator is used to Multiply 2 integers
+
+print("5 / 6 is ", 25/6)
+# / operator is used to Divide 2 integers
+
+print("25 // 6 is ", 25//6)
+# // operator is used to divide 2 integers and just show the number before decimal
+# like 25 /6 is 4.166666666666667 but it shows only 4
+
+print("5 ** 3 is ", 5**3)
+# ** Operator act as 5^3
+
+print("5 % 6 is ", 5 % 6)
+# % operator is used to find the reminder of 2 divisors
